b26e24bb9d5352ab7eb4a85e54642f28e0b6cb69
Ashish Kumar Singh <ashishkumarsingh046@gmail.com>
2019-01-08 14:17:48 -0500

    Implementation of Bubble Sort in java

--- Sorting Algorithms/BubbleSort.java
+++ Sorting Algorithms/BubbleSort.java
@@ -6,31 +6,39 @@
 #
 # Author            : Ashish Singh
 #
-# Date created      : 20181215
+# Date created      : 20190107
 #
 # Purpose           : Implementing bubble sort algorithm in Java
 #
 # **********************************************************************;
 */
 
-// Java program for implementation of Bubble Sort
+// Class that contains the bubbleSort function
 class BubbleSort
 {
+
+    // Function that implements Bubble Sort algorithm
     void bubbleSort(int unsorted_array[])
     {
+        // Get size of array to execute loop
         int array_size = unsorted_array.length;
+
+        // Outer loop to select the element to be compared
         for (int i = 0; i < array_size-1; i++)
+
+            // Inner loop to compare all other elements with the selected element
             for (int j = 0; j < array_size-i-1; j++)
+
+                // If jth element is greater than (j+1)th element then interchange positions
                 if (unsorted_array[j] > unsorted_array[j+1])
                 {
-                    // swap arr[j+1] and arr[i]
                     int temp = unsorted_array[j];
                     unsorted_array[j] = unsorted_array[j+1];
                     unsorted_array[j+1] = temp;
                 }
     }
 
-    /* Prints the array */
+    // Function to print the sorted array as a console output
     void printArray(int sorted_array[])
     {
         int array_size = sorted_array.length;
@@ -39,7 +47,7 @@ class BubbleSort
         System.out.println();
     }
 
-    // Driver method to test above
+    // Main function to execute the Bubble Sort algorithm.
     public static void main(String args[])
     {
         BubbleSort ob = new BubbleSort();
